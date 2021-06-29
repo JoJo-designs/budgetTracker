0ac501c49d960ea64671c5d01b0c8b61547a3abe
jojo <jodiladouceur.design@gmail.com>
2021-06-29 09:17:02 -0400

    edits to server and added a service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,3 +2,29 @@ console.log("service worker is working");
 
 const CACHE_NAME = "static-cache";
 const DATA_CACHE_NAME = "data-cache";
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/icons/icon-1.png',
+    '/icons/icon-2.png',
+    '/icons/icon-192x192.png'
+];
+
+// install
+self.addEventListener("install", function(evt) {
+    console.log(evt);
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  
