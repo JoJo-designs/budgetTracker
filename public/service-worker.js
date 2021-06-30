78cb9f204db5cf6a0d2363c1d918603e76ce106b
jojo <jodiladouceur.design@gmail.com>
2021-06-30 10:01:40 -0400

    edits to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,11 +4,12 @@ const CACHE_NAME = "static-cache";
 const DATA_CACHE_NAME = "data-cache";
 
 // I attempt to add these files the first two are the issue. The index.html works alone but not with the
-// other files I don't know why but I guess I am part way there.
+// other files I don't know why but I guess I am part way there. I will go to office hours 
+// for this one
 const FILES_TO_CACHE = [
     // '/',
     // '/index.html',
-    '/style.css',
+    // '/style.css',
     '/index.js',
     '/manifest.webmanifest',
     '/icons/icon-1.png',
@@ -21,19 +22,25 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
       );
-
-    // event.waitUntil(
-    //     caches.open('static-cache').then((cache) => {
-    //         return cache.add([
-    //             '/',
-    //             '/index.html',
-    //             '/style.css',
-    //             '/index.js',
-    //             '/manifest.webmanifest',
-    //             '/icons/icon-1.png',
-    //             '/icons/icon-2.png',
-    //             '/icons/icon-192x192.png'
-    //         ]);
-    //     })
-    // );
 });
+
+// listen to the active stage of the app  I think removes old
+// caches if the names don't match.
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !==DATA_CACHE_NAME) {
+                        console.log("Removeing old cache", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+
