62ba2e953e0607f103fc0284caed980388383729
jojo <jodiladouceur.design@gmail.com>
2021-07-08 09:16:33 -0400

    edits to db.js

--- public/db.js
+++ public/db.js
@@ -1,12 +1,41 @@
-export function useIndexDb() {
+// okay I need to build an indexDb 
+// the indexDb is connected to the index.js on line 142 with a function 
+// called saveRecord.
 
-let db;
+// not sure which activity to look at to get info on checking if the 
+// app is online again.
 
-const request = window.indexedDB.open("Transactions", 1);
+export function saveRecord(transactions, budget, method, transaction) {
+    const request = window.indexedDB.open(transactions, 1);
+    let db,
+    item,
+    store;
 
-request.onupgradeneeded = (event) => {
-    const db = request.results;
-    db.createObjectStore(storeName, { keyPath: "_id" });
-}
+    request.onupgradeneeded = (event) => {
+        const db = request.result;
+        db.createObjectStore(budget, {keyPath: "id"});
+    };
 
+    request.onerror = (event) => {
+        console.log("We ran into an error")
+    };
+
+    request.onsuccess = (event) => {
+        db = request.result;
+        item = db.transaction(budget, "readwrite");
+        store = item.odjectStore(budget);
+
+        db.onerror = (event) => {
+            console.log("we ran into an error");
+        };
+        if (method === "put") {
+            store.put(transaction);
+        }
+        if (method === "get") {
+            store.getAll();
+        }
+        item.oncomplete = () => {
+            db.close();
+        }
+    };
 }
